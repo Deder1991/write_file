93e2ce400727d68d684952219e43f47915f57c18
Deder1991 <64907118+Deder1991@users.noreply.github.com>
2020-05-12 15:35:46 +0800

    增加讀取功能

--- write_file.py
+++ write_file.py
@@ -1,12 +1,30 @@
 products = []
-while True:
-	food = input('請輸入食物名稱:')
-	if food == 'q':
-		break
-	price = input('請輸入價格:')
-	products.append([food, price])
+count = 2
+
+with open('products.csv', 'r', encoding = 'utf-8') as f:
+	for line in f:
+		if '商品名稱,價格' in line:
+			continue
+		name, price = line.strip().split(',')
+		products.append([name,price])
+print(products)
+		
+
+while count > 0:
+	name = input('請輸入物品名稱:')
+	price = input('請輸入物品價格:')
+	products.append([name, price])
+	#也可以寫成
+	# p = []
+	# p.append(name)
+	# p.append(price)
+	# products.append(p)
+	count -=1
 print(products)
 
-with open('products.csv', 'w') as f:
+with open('products.csv', 'w', encoding = 'utf-8') as f:
+	f.write('商品名稱,價格\n')
 	for line in products:
 		f.write(line[0] + ',' + line[1] + '\n')
+
+
